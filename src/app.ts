35b5a26852e79d1f6ce802cb1773abbe38c56b23
Md. Salauddin <salauddinahmed555@gmail.com>
2024-11-17 21:02:05 +0600

    Initialized the project

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+console.log(process.cwd())
+// D:\Level-2-projects\ML-2\Moongoose
