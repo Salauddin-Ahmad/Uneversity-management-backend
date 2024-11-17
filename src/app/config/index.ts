c245b2450a7bd230a8a90daf1c980c46091106e5
Md. Salauddin <salauddinahmed555@gmail.com>
2024-11-18 00:51:06 +0600

    basic project setup done with typescripy

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -2,11 +2,9 @@ import dotenv from 'dotenv';
 
 import path from 'path';
 
+dotenv.config({ path: path.join((process.cwd(), '.env')) });
 
-
-dotenv.config({ path: path.join((process.cwd(), '.env' ))});
-
-export default{
-    port: process.env.PORT,
-    database_url: process.env.DARABASE_URL
-}
+export default {
+  port: process.env.PORT,
+  database_url: process.env.DARABASE_URL,
+};
