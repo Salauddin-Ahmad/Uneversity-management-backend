92cb1f7d30715d2d6e01096b1e94a98ab949f00b
Md. Salauddin <salauddinahmed555@gmail.com>
2024-11-19 02:03:52 +0600

    finally connceted 2 mongodb after 3 hours of debugging

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -4,7 +4,12 @@ import path from 'path';
 
 dotenv.config({ path: path.join((process.cwd(), '.env')) });
 
+// Log to check if environment variables are loaded
+console.log('Loaded PORT:', process.env.PORT);
+console.log('Loaded DATABASE_URL:', process.env.DATABASE_URL);
+
+
 export default {
   port: process.env.PORT,
-  database_url: process.env.DARABASE_URL,
+  database_url: process.env.DATABASE_URL,
 };
