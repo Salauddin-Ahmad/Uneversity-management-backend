421bff068ebbb9e546d9bae102cd9e59a82e63af
Md. Salauddin <salauddinahmed555@gmail.com>
2024-12-02 18:46:47 +0600

    refactored the routes

---
+++ src/app/routes/index.ts
@@ -0,0 +1,28 @@
+import { Router } from "express";
+import { StudentRoutes } from "../modules/student/student.route";
+import { UserRoutes } from "../modules/user/user.route";
+
+const router = Router();
+
+const moduleRoutes = [
+    {
+        path: '/users',
+        module: UserRoutes
+    },
+    {
+        path: '/students',
+        module: StudentRoutes
+    }
+]
+
+
+moduleRoutes.forEach(({ path, module }) => {
+    router.use(path, module);
+}); 
+
+
+router.use('/users', UserRoutes);
+router.use('/students', StudentRoutes)
+
+
+export default router 
