834ee7c269b3ff46c7324962337dc6a9d3029b9c
Md. Salauddin <salauddinahmed555@gmail.com>
2024-12-14 23:15:26 +0600

    Error Patterns in Zod and Mongoose

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,17 +1,49 @@
 import { NextFunction, Request, Response } from 'express';
+import { ZodError } from 'zod';
 
+const globalErrorHandler = (
+  error: any,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  // setting default values
+  let statusCode = error.statusCode || 500;
+  let message = error.message || 'Something went wrong!';
 
+  if(error instanceof ZodError){
+    statusCode = 400
+    message = 'ami zod error'
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  type TerrorSource = {
+    path: string | number;
+    message: string;
+  }[];
 
-const globalErrorHandler =  (error: any, req: Request, res: Response, next: NextFunction) => {
-  const statusCode = error.statusCode || 500;
-  const message = error.message || "Something went wrong!";
+  let errorSources: TerrorSource = [{
+    path: '',
+    message: '',
+  }];
+
+  
+
   res.status(statusCode).json({
     success: false,
     message,
-    error: error,
+    errorSources
   });
   next(error);
 };
 
-
-  export default globalErrorHandler;
+export default globalErrorHandler;
